761d0c3f602e44c4488358172f975485c6158a3d
Ariel Hernan Morales Segales <ahmsarielmorales@gmail.com>
2025-05-18 22:16:02 -0400

    Refactorización y mejoras de estilo con temática pug

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { routesIDs } from './services/routes-ids';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Server
+{
+   path: 'producto/:id',
+   renderMode: RenderMode.Prerender,
+   async getPrerenderParams() {
+      const ids = routesIDs;
+      return ids.map(id => ({ id }));
+  },
+ },
+ {
+   path: '**',
+   renderMode: RenderMode.Prerender
   }
 ];
