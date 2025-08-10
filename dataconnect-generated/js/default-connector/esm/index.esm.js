5deda9f5907000be80eff538a1709bb1c2b19391
Ariel Hernan Morales Segales <ahmsarielmorales@gmail.com>
2025-08-10 18:28:32 -0400

    feat: Add contact page component

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'catalogo',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'catalogo',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
